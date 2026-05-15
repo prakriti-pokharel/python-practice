a0844a3a1dd1524ce801873d3fa9c4aee95dab63
Prakriti Pokharel <prakritipokharel2075@gmail.com>
2026-05-15 16:47:51 +0545

    Fix merge conflict in day01

--- day01.py
+++ day01.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Creating a variable
 name = "Prakriti Pokharel"
 
@@ -9,26 +8,9 @@ my_list = ["python","pandas","numpy","ml"]
 for x in my_list:
     print(x)
 
-#Funtion that takes two numbers and returns their average
+#Function that takes two numbers and returns their average
 def avg(a, b):
     return (a+b)/2
 
 #Calling the function to print result
-=======
-#Creating a variable
-name = "Prakriti Pokharel"
-
-#List of things I want to learn this year
-my_list = ["python","pandas","numpy","ml"]
-
-#Loop to print each item in the list
-for x in my_list:
-    print(x)
-
-#Funtion that takes two numbers and returns their average
-def avg(a, b):
-    return (a+b)/2
-
-#Calling the function to print result
->>>>>>> e6773dcf341555bf3d3620de4aa04a02732d08d9
 print(avg(8,2))
