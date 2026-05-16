885aa4f0f0e8f7da65e582008680bfb238c6e8b7
Prakriti Pokharel <prakritipokharel2075@gmail.com>
2026-05-16 15:10:43 +0545

    Add day03 script - user input, conditionals, even/odd function

---
+++ day03.py
@@ -0,0 +1,20 @@
+#Ask user to enter a number
+n= int(input ("Enter a number: "))
+
+#Condition
+if n> 10:
+    print(n,"is greater than 10")
+elif n<10:
+    print(n,"is not greater than 10")
+else:
+    print(n,"is equal to 10")
+
+#Function returning even/odd
+def odd_even(n):
+    if n %2 ==0:
+        return str(n) + "is Even"
+    else:
+        return str(n) + "is Odd"
+
+#Calling the function
+print(odd_even(n))
